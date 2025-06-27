ce8cffce1c55128ace4abd6ed42e48d3904ad091
Ashish Kumar Jha <79639825+Ashish-Github193@users.noreply.github.com>
2025-06-27 19:58:54 +0530

    removed harpoon-config

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -138,16 +138,6 @@ local plugins = {
     end,
   },
 
-  -- Quick file navigation
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local harpoon = require("harpoon")
-      harpoon:setup()
-    end,
-  },
 
   -- LSP installer
   { 
