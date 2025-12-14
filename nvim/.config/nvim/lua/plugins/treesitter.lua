d1416ac2671e856bed4a9551f7e488aad81a4660
Ashish Kumar Jha <79639825+Ashish-Github193@users.noreply.github.com>
2025-12-14 18:39:35 +0000

    fix: do not immediately run plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,13 @@
-require("nvim-treesitter.configs").setup({
-	ensure_installed = { "python", "lua", "javascript", "typescript" },
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-})
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "python", "lua", "javascript", "typescript" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
+}
